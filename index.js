10ee21cb0abb0bd619ce8e97aba1c4008536c732
“Tamim239” <mdibrahimislamtamim239@gmail.com>
2024-05-11 08:44:17 +0600

    setup crud in read operaton

--- index.js
+++ index.js
@@ -38,6 +38,11 @@ async function run() {
     // Send a ping to confirm a successful connection
        const jobsCollection = await client.db('thrivehive').collection('services')
 
+       app.get('/postJobs', async(req, res) =>{
+        const result = await jobsCollection.find().toArray()
+        res.send(result) 
+       })
+
 
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
